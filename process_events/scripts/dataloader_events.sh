6c81604e54651bf4d48965770dc9e849f09cba8e
ChunTao1999 <tao88@purdue.edu>
2023-08-04 10:22:10 -0400

    multi-modal dataset

--- process_events/scripts/dataloader_events.sh
+++
@@ -1,8 +0,0 @@
-DATA_PATH=/home/nano01/a/tao88/shared_folder/CV/events_7.26
-CAL_DATA_PATH=/home/nano01/a/tao88/shared_folder/CV/events_7.26
-DOWNLOAD_CSV=0
-
-python3 /home/nano01/a/tao88/cav-mae/process_events/dataloader_events.py \
--d ${DATA_PATH} \
---cal-data-path ${CAL_DATA_PATH} \
---download-csvs ${DOWNLOAD_CSV}
